0adcc7ddbd87bc63b1967b8b9d43a08a6c3e59cd
mellonaut <rleemattingly@outlook.com>
2023-06-28 12:13:17 -0400

    images

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    // Handle the click event here
+    // You can redirect to another page or perform any other action
+    alert('Link clicked!');
+  });
+  
